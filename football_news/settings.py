58a73c0d1d0a55721651991f3cdb91eaa8f028fc
Mednoob <ahmadyaqdhan123@gmail.com>
2025-08-27 15:26:14 +0900

    Fix issue dari penggunaan petik

--- football_news/settings.py
+++ football_news/settings.py
@@ -89,7 +89,7 @@ if PRODUCTION:
             "HOST": os.getenv("DB_HOST"),
             "PORT": os.getenv("DB_PORT"),
             "OPTIONS": {
-                "options": f"-c search_path={os.getenv("SCHEMA", "public")}"
+                "options": f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
         }
     }
